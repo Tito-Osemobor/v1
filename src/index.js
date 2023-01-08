e4df1e05b3c3266e0f5297e95893b5b2a4140117
Tito-Osemobor <titoosemobor@gmail.com>
2023-01-07 21:57:45 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".navbar-links");
+    let currentColor = "6c757d";
 
     burger.addEventListener("click", () => {
         nav.classList.toggle("navbar-links-active");
         document.body.classList.toggle("overflow-hidden");
+        if (currentColor === "6c757d") {
+            currentColor = "495057";
+        } else {
+            currentColor = "6c757d";
+        }
+        document.body.classList.add('transition');
+        document.body.classList.toggle("overflow-hidden");
+        document
+            .querySelector('meta[name="theme-color"]')
+            .setAttribute("content", currentColor);
     });
 };
 
