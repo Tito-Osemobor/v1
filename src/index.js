f528e504200d744e5bacfa9247cc4b0cdb60744d
Tito-Osemobor <titoosemobor@gmail.com>
2023-01-07 23:45:07 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".navbar-links");
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".navbar-links");
 
+const navSlide = () => {
     burger.addEventListener("click", () => {
         nav.classList.toggle("navbar-links-active");
         document.body.classList.toggle("overflow-hidden");
     });
 };
+
+window.addEventListener("click", (event) => {
+    if (!event.target.closest("nav")) {
+        nav.classList.toggle("navbar-links-active", false);
+        document.body.classList.toggle("overflow-hidden", false);
+    }
+});
+
+window.addEventListener("touchstart", (event) => {
+    if (!event.target.closest("nav")) {
+        nav.classList.toggle("navbar-links-active", false);
+        document.body.classList.toggle("overflow-hidden", false);
+    }
+});
 
 navSlide();
