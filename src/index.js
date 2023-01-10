e73b2a686278782a90c89f305b1399ca1839c015
Tito-Osemobor <titoosemobor@gmail.com>
2023-01-10 09:43:09 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const navSlide = () => {
     navToggle.addEventListener("click", () => {
         navToggle.classList.toggle("is-active");
         primaryNav.classList.toggle("is-active");
+        document.body.classList.toggle("overflow-hidden");    
+        document.querySelector("main").classList.toggle("blur");
     });
 };
 
@@ -23,5 +25,14 @@ const navSlide = () => {
 //         document.querySelector("main").classList.toggle("blur", false);
 //     }
 // });
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+});
 
 navSlide();
