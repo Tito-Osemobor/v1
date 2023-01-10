fe57b393aa81ff137bb09efaa509450ec8409bc5
Tito-Osemobor <titoosemobor@gmail.com>
2023-01-10 01:38:29 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -3,25 +3,39 @@ const nav = document.querySelector(".navbar-links");
 
 const navSlide = () => {
     burger.addEventListener("click", () => {
+        burger.classList.toggle("is-active");
         nav.classList.toggle("navbar-links-active");
-        document.body.classList.toggle("overflow-hidden");
-        document.querySelector('main').classList.toggle("blur");
+        document.querySelector("main").classList.toggle("blur");
     });
 };
 
 window.addEventListener("click", (event) => {
     if (!event.target.closest("nav")) {
+        burger.classList.toggle("is-active", false);
         nav.classList.toggle("navbar-links-active", false);
-        document.body.classList.toggle("overflow-hidden", false);
-        document.querySelector('main').classList.toggle("blur", false);
+        document.querySelector("main").classList.toggle("blur", false);
+        if (nav.classList.contains("transition")) {
+            setTimeout(function () {
+                nav.classList.remove("transition");
+            }, 500);
+        } else {
+            nav.classList.add("transition");
+        }
     }
 });
 
 window.addEventListener("touchstart", (event) => {
     if (!event.target.closest("nav")) {
+        burger.classList.toggle("is-active", false);
         nav.classList.toggle("navbar-links-active", false);
-        document.body.classList.toggle("overflow-hidden", false);
-        document.querySelector('main').classList.toggle("blur", false);
+        document.querySelector("main").classList.toggle("blur", false);
+        if (nav.classList.contains("transition")) {
+            setTimeout(function () {
+                nav.classList.remove("transition");
+            }, 500);
+        } else {
+            nav.classList.add("transition");
+        }
     }
 });
 
