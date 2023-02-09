bf7ae95c76a3db041948dc3193846c60003d4966
Tito-Osemobor <titoosemobor@gmail.com>
2023-02-09 13:23:35 -0500

    Testing

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -2,7 +2,8 @@ const loader = document.querySelector(".loader-wrapper");
 const content = document.querySelector(".content");
 const brick = document.querySelector(".hex-brick");
 
-setTimeout(function () {
-  loader.style.display = "none";
-  content.style.display = "block";
-}, 4000);
+// setTimeout(function () {
+//   loader.style.display = "none";
+//   content.style.display = "block";
+//   document.body.classList.remove("overflow-hidden");
+// }, 4000);
