bc2787b69cd75512a61ef0de8d815d789ff5b5c2
Tito-Osemobor <titoosemobor@gmail.com>
2023-02-13 22:12:13 -0500

    Fixed animation issue

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -1,10 +1,49 @@
-// // const loader = document.querySelector(".loader-wrapper");
-// const content = document.querySelector(".content");
-// const logo = document.querySelector(".logo");
-// // const brick = document.querySelector(".hex-brick");
+const loader = document.querySelector(".loader-wrapper");
 
-// setTimeout(function () {
-//     logo.style.visibility = "visible";
-//     document.body.classList.remove("overflow-hidden");
-// }, 1000);
+setTimeout(function () {
+    // loader.style.display = "none";
+    document.body.style.overflowY = "scroll";
+    AOS.init();
+}, 4000);
 
+var fadeInInterval, fadeOutInterval;
+
+window.addEventListener("load", function () {
+    clearInterval(fadeInInterval);
+    clearInterval(fadeOutInterval);
+    loader.fadeIn = function (timing) {
+        var newValue = 0;
+        loader.style.display = "block";
+        loader.style.opacity = 0;
+        fadeInInterval = setInterval(function () {
+            if (newValue < 1) {
+                newValue += 0.01;
+            } else if (newValue === 1) {
+                clearInterval(fadeInInterval);
+            }
+            loader.style.opacity = newValue;
+        }, timing);
+    };
+    loader.fadeIn(6);
+});
+
+setTimeout (function () {
+    clearInterval(fadeInInterval);
+    clearInterval(fadeOutInterval);
+    loader.fadeOut = function (timing) {
+        var newValue = 1;
+        loader.style.opacity = 1;
+        fadeOutInterval = setInterval(function () {
+            if (newValue > 0) {
+                newValue -= 0.01;
+            } else if (newValue < 0) {
+                loader.style.opacity = 0;
+                loader.style.display = "none";
+                clearInterval(fadeOutInterval);
+            }
+            loader.style.opacity = newValue;
+        }, timing);
+    };
+    loader.fadeOut(6);
+    loader.style.display = "none";
+}, 4000);
