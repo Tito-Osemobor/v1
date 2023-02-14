8e1536e72277b5f68eb885cfd470a449a670d417
Tito-Osemobor <titoosemobor@gmail.com>
2023-02-13 19:02:44 -0500

    Updates

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -1,10 +1,10 @@
-// const loader = document.querySelector(".loader-wrapper");
-const content = document.querySelector(".content");
-const logo = document.querySelector(".logo");
-// const brick = document.querySelector(".hex-brick");
+// // const loader = document.querySelector(".loader-wrapper");
+// const content = document.querySelector(".content");
+// const logo = document.querySelector(".logo");
+// // const brick = document.querySelector(".hex-brick");
 
-setTimeout(function () {
-    logo.style.visibility = "visible";
-    document.body.classList.remove("overflow-hidden");
-}, 1000);
+// setTimeout(function () {
+//     logo.style.visibility = "visible";
+//     document.body.classList.remove("overflow-hidden");
+// }, 1000);
 
